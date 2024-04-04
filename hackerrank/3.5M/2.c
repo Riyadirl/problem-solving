a821f612f349f074c30d9ef876b4f4e612021a34
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-04-04 15:55:19 +0600

    first commit

---
+++ hackerrank/3.5M/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int A, B;
+    int sum = 0;
+    scanf("%d %d", &A, &B);
+    printf("%d", sum = A + B);
+    return 0;
+}
