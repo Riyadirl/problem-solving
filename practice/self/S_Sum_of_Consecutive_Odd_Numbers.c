9d8e7def5f31bcc20a85790ef6a46deb1e078a43
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-04-19 07:39:17 +0600

    .

---
+++ practice/self/S_Sum_of_Consecutive_Odd_Numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i < n; i++)
+    {
+        int x, y;
+        scanf("%d %d\n", &x, &y);
+    }
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i > x && i < y)
+        {
+            if (i % 2 == 1)
+            {
+                sum += i;
+            }
+        }
+        printf("%d\n", sum);
+    }
+
+    return 0;
+}
