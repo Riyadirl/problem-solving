791a8ee6fbb4da9d96ac07a2f24c7fb531a890c4
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-04-30 18:25:18 +0600

    .

---
+++ codeforces/c/14.5(nestedLoop+pattern)/T_Shape_2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int s = n - 1, k = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        // space
+        for (int j = 1; j <= s; j++)
+        {
+            printf(" ");
+        }
+        //*
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        s--;
+        k += 2;
+    }
+
+    return 0;
+}
