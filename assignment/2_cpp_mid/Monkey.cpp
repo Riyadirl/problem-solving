865dcc7e0b5dc89a1cff662106c8fa6e570cbc7d
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2024-05-25 11:45:29 +0600

    cpp_mid

--- assignment/2_cpp_mid/Monkey.cpp
+++ assignment/2_cpp_mid/Monkey.cpp
@@ -1,12 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
-    string line;
-    while (getline(cin, line))
+int main()
+{
+    char s[100001];
+
+    while (cin.getline(s, 100001))
     {
-        
+        int size = strlen(s);
+        sort(s, s + size);
+        for (int i = 0; i < size; i++)
+        {
+            if (s[i] != ' ')
+            {
+                cout << s[i];
+            }
+        }
+        cout << endl;
     }
+
     return 0;
 }
